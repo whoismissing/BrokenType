907c70ac617fc23ca31e265a3d6c01d130e32942
whoismissing <whereismissing@gmail.com>
2024-03-26 12:56:14 -0400

    convert font2pdf scripts to py3

--- font2pdf/ttfotf_to_pdf.py
+++ font2pdf/ttfotf_to_pdf.py
@@ -29,109 +29,114 @@ PAGE_CONTENTS_IDX_START = 200
 
 def main(argv):
   if len(argv) != 3:
-    print "Usage: %s <font file> <output .pdf path>" % argv[0]
+    print("Usage: %s <font file> <output .pdf path>" % argv[0])
     sys.exit(1)
 
   # Obtain the number of glyphs in the font, and calculate the number of necessary pages.
   font = TTFont(argv[1])
   glyph_count = len(font.getGlyphSet())
-  print "Glyphs in font: %d" % glyph_count
+  print("Glyphs in font: %d" % glyph_count)
 
   page_count = int(math.ceil(float(glyph_count) / GLYPHS_PER_PAGE))
-  print "Generated pages: %d" % page_count
+  print("Generated pages: %d" % page_count)
 
   # Craft the PDF header.
-  pdf_data = "%PDF-1.1\n"
-  pdf_data += "\n"
-  pdf_data += "1 0 obj\n"
-  pdf_data += "<< /Pages 2 0 R >>\n"
-  pdf_data += "endobj\n"
-  pdf_data += "\n"
-  pdf_data += "2 0 obj\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /Type /Pages\n"
-  pdf_data += "  /Count %d\n" % page_count
-  pdf_data += "  /Kids [ %s ]\n" % " ".join(map(lambda x:" %d 0 R" % (PAGE_OBJECT_IDX_START + x), (i for i in xrange(page_count))))
-  pdf_data += ">>\n"
-  pdf_data += "endobj\n"
-  pdf_data += "\n"
+  pdf_data = b"%PDF-1.1\n"
+  pdf_data += b"\n"
+  pdf_data += b"1 0 obj\n"
+  pdf_data += b"<< /Pages 2 0 R >>\n"
+  pdf_data += b"endobj\n"
+  pdf_data += b"\n"
+  pdf_data += b"2 0 obj\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /Type /Pages\n"
+  pdf_data += b"  /Count %d\n" % page_count
+  pdf_data += b"  /Kids [ %s ]\n" % b" ".join(map(lambda x:b" %d 0 R" % (PAGE_OBJECT_IDX_START + x), (i for i in range(page_count))))
+  pdf_data += b">>\n"
+  pdf_data += b"endobj\n"
+  pdf_data += b"\n"
 
   # Construct the page descriptor objects.
-  for i in xrange(page_count):
-    pdf_data += "%d 0 obj\n" % (PAGE_OBJECT_IDX_START + i)
-    pdf_data += "<<\n"
-    pdf_data += "  /Type /Page\n"
-    pdf_data += "  /MediaBox [0 0 612 792]\n"
-    pdf_data += "  /Contents %d 0 R\n" % (PAGE_CONTENTS_IDX_START + i)
-    pdf_data += "  /Parent 2 0 R\n"
-    pdf_data += "  /Resources <<\n"
-    pdf_data += "    /Font <<\n"
-    pdf_data += "      /CustomFont <<\n"
-    pdf_data += "        /Type /Font\n"
-    pdf_data += "        /Subtype /Type0\n"
-    pdf_data += "        /BaseFont /TestFont\n"
-    pdf_data += "        /Encoding /Identity-H\n"
-    pdf_data += "        /DescendantFonts [4 0 R]\n"
-    pdf_data += "      >>\n"
-    pdf_data += "    >>\n"
-    pdf_data += "  >>\n"
-    pdf_data += ">>\n"
-    pdf_data += "endobj\n"
+  for i in range(page_count):
+    pdf_data += b"%d 0 obj\n" % (PAGE_OBJECT_IDX_START + i)
+    pdf_data += b"<<\n"
+    pdf_data += b"  /Type /Page\n"
+    pdf_data += b"  /MediaBox [0 0 612 792]\n"
+    pdf_data += b"  /Contents %d 0 R\n" % (PAGE_CONTENTS_IDX_START + i)
+    pdf_data += b"  /Parent 2 0 R\n"
+    pdf_data += b"  /Resources <<\n"
+    pdf_data += b"    /Font <<\n"
+    pdf_data += b"      /CustomFont <<\n"
+    pdf_data += b"        /Type /Font\n"
+    pdf_data += b"        /Subtype /Type0\n"
+    pdf_data += b"        /BaseFont /TestFont\n"
+    pdf_data += b"        /Encoding /Identity-H\n"
+    pdf_data += b"        /DescendantFonts [4 0 R]\n"
+    pdf_data += b"      >>\n"
+    pdf_data += b"    >>\n"
+    pdf_data += b"  >>\n"
+    pdf_data += b">>\n"
+    pdf_data += b"endobj\n"
 
   # Construct the font body object.
   with open(argv[1], "rb") as f:
       font_data = f.read()
 
-  pdf_data += "3 0 obj\n"
-  pdf_data += "<</Subtype /OpenType/Length %d>>stream\n" % len(font_data)
+  pdf_data += b"3 0 obj\n"
+  pdf_data += b"<</Subtype /OpenType/Length %d>>stream\n" % len(font_data)
   pdf_data += font_data
-  pdf_data += "\nendstream\n"
-  pdf_data += "endobj\n"
+  pdf_data += b"\nendstream\n"
+  pdf_data += b"endobj\n"
   
   # Construct the page contents objects.
-  for i in xrange(page_count):
-    pdf_data += "%d 0 obj\n" % (PAGE_CONTENTS_IDX_START + i)
-    pdf_data += "<< >>\n"
-    pdf_data += "stream\n"
-    pdf_data += "BT\n"
+  for i in range(page_count):
+    pdf_data += b"%d 0 obj\n" % (PAGE_CONTENTS_IDX_START + i)
+    pdf_data += b"<< >>\n"
+    pdf_data += b"stream\n"
+    pdf_data += b"BT\n"
 
-    for j in xrange(SEGMENTS_PER_PAGE):
+    for j in range(SEGMENTS_PER_PAGE):
       if i * GLYPHS_PER_PAGE + j * GLYPHS_PER_SEGMENT >= glyph_count:
         break
 
-      pdf_data += "  /CustomFont 12 Tf\n"
+      pdf_data += b"  /CustomFont 12 Tf\n"
       if j == 0:
-        pdf_data += "  10 775 Td\n"
+        pdf_data += b"  10 775 Td\n"
       else:
-        pdf_data += "  0 -10 Td\n"
-      pdf_data += "  <%s> Tj\n" % "".join(map(lambda x: struct.pack(">H", x), (x for x in xrange(i * GLYPHS_PER_PAGE + j * GLYPHS_PER_SEGMENT, i * GLYPHS_PER_PAGE + (j + 1) * GLYPHS_PER_SEGMENT)))).encode("hex")
+        pdf_data += b"  0 -10 Td\n"
 
-    pdf_data += "ET\n"
-    pdf_data += "endstream\n"
-    pdf_data += "endobj\n"
+      # Example:
+      # <00320033003400350036003700380039003a003b003c003d003e003f0040004100420043004400450046004700480049004a004b004c004d004e004f0050005100520053005400550056005700580059005a005b005c005d005e005f0060006100620063> Tj
+      glyphs = b"".join(map(lambda x: struct.pack(">H", x), (x for x in range(i * GLYPHS_PER_PAGE + j * GLYPHS_PER_SEGMENT, i * GLYPHS_PER_PAGE + (j + 1) * GLYPHS_PER_SEGMENT))))
+      hex_str = "".join(["%02x" % b for b in glyphs]).encode("utf-8")
+      pdf_data += b"  <%s> Tj\n" % hex_str
+
+    pdf_data += b"ET\n"
+    pdf_data += b"endstream\n"
+    pdf_data += b"endobj\n"
 
   # Construct the descendant font object.
-  pdf_data += "4 0 obj\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /FontDescriptor <<\n"
-  pdf_data += "    /Type /FontDescriptor\n"
-  pdf_data += "    /FontName /TestFont\n"
-  pdf_data += "    /Flags 5\n"
-  pdf_data += "    /FontBBox[0 0 10 10]\n"
-  pdf_data += "    /FontFile3 3 0 R\n"
-  pdf_data += "  >>\n"
-  pdf_data += "  /Type /Font\n"
-  pdf_data += "  /Subtype /OpenType\n"
-  pdf_data += "  /BaseFont /TestFont\n"
-  pdf_data += "  /Widths [1000]\n"
-  pdf_data += ">>\n"
-  pdf_data += "endobj\n"
+  pdf_data += b"4 0 obj\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /FontDescriptor <<\n"
+  pdf_data += b"    /Type /FontDescriptor\n"
+  pdf_data += b"    /FontName /TestFont\n"
+  pdf_data += b"    /Flags 5\n"
+  pdf_data += b"    /FontBBox[0 0 10 10]\n"
+  pdf_data += b"    /FontFile3 3 0 R\n"
+  pdf_data += b"  >>\n"
+  pdf_data += b"  /Type /Font\n"
+  pdf_data += b"  /Subtype /OpenType\n"
+  pdf_data += b"  /BaseFont /TestFont\n"
+  pdf_data += b"  /Widths [1000]\n"
+  pdf_data += b">>\n"
+  pdf_data += b"endobj\n"
 
   # Construct the trailer.
-  pdf_data += "trailer\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /Root 1 0 R\n"
-  pdf_data += ">>\n"
+  pdf_data += b"trailer\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /Root 1 0 R\n"
+  pdf_data += b">>\n"
 
   # Write the output to disk.
   with open(argv[2], "w+b") as f:
--- font2pdf/type1_to_pdf.py
+++ font2pdf/type1_to_pdf.py
@@ -25,99 +25,101 @@ GLYPHS_PER_SEGMENT = 50
 
 def main(argv):
   if len(argv) != 3:
-    print "Usage: %s <.pfb font file> <output .pdf path>" % argv[0]
+    print("Usage: %s <.pfb font file> <output .pdf path>" % argv[0])
     sys.exit(1)
 
   # Load the number of glyphs in the font.
   font = T1Font(argv[1])
   glyph_count = len(font.getGlyphSet())
-  print "Glyphs in font: %d" % glyph_count
+  print("Glyphs in font: %d" % glyph_count)
 
   # Craft the PDF header.
-  pdf_data = "%PDF-1.1\n"
-  pdf_data += "\n"
-  pdf_data += "1 0 obj\n"
-  pdf_data += "<< /Pages 2 0 R >>\n"
-  pdf_data += "endobj\n"
-  pdf_data += "\n"
-  pdf_data += "2 0 obj\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /Type /Pages\n"
-  pdf_data += "  /Count 1\n"
-  pdf_data += "  /Kids [ 3 0 R ]\n"
-  pdf_data += ">>\n"
-  pdf_data += "endobj\n"
-  pdf_data += "\n"
-  pdf_data += "3 0 obj\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /Type /Page\n"
-  pdf_data += "  /MediaBox [0 0 612 792]\n"
-  pdf_data += "  /Contents 5 0 R\n"
-  pdf_data += "  /Parent 2 0 R\n"
-  pdf_data += "  /Resources <<\n"
-  pdf_data += "    /Font <<\n"
-  pdf_data += "      /CustomFont <<\n"
-  pdf_data += "        /Type /Font\n"
-  pdf_data += "        /Subtype /Type0\n"
-  pdf_data += "        /BaseFont /TestFont\n"
-  pdf_data += "        /Encoding /Identity-H\n"
-  pdf_data += "        /DescendantFonts [6 0 R]\n"
-  pdf_data += "      >>\n"
-  pdf_data += "    >>\n"
-  pdf_data += "  >>\n"
-  pdf_data += ">>\n"
-  pdf_data += "endobj\n"
+  pdf_data = b"%PDF-1.1\n"
+  pdf_data += b"\n"
+  pdf_data += b"1 0 obj\n"
+  pdf_data += b"<< /Pages 2 0 R >>\n"
+  pdf_data += b"endobj\n"
+  pdf_data += b"\n"
+  pdf_data += b"2 0 obj\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /Type /Pages\n"
+  pdf_data += b"  /Count 1\n"
+  pdf_data += b"  /Kids [ 3 0 R ]\n"
+  pdf_data += b">>\n"
+  pdf_data += b"endobj\n"
+  pdf_data += b"\n"
+  pdf_data += b"3 0 obj\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /Type /Page\n"
+  pdf_data += b"  /MediaBox [0 0 612 792]\n"
+  pdf_data += b"  /Contents 5 0 R\n"
+  pdf_data += b"  /Parent 2 0 R\n"
+  pdf_data += b"  /Resources <<\n"
+  pdf_data += b"    /Font <<\n"
+  pdf_data += b"      /CustomFont <<\n"
+  pdf_data += b"        /Type /Font\n"
+  pdf_data += b"        /Subtype /Type0\n"
+  pdf_data += b"        /BaseFont /TestFont\n"
+  pdf_data += b"        /Encoding /Identity-H\n"
+  pdf_data += b"        /DescendantFonts [6 0 R]\n"
+  pdf_data += b"      >>\n"
+  pdf_data += b"    >>\n"
+  pdf_data += b"  >>\n"
+  pdf_data += b">>\n"
+  pdf_data += b"endobj\n"
 
   # Construct the font body object.
   with open(argv[1], "rb") as f:
     font_data = f.read()
 
-  pdf_data += "4 0 obj\n"
-  pdf_data += "<</Subtype /Type1/Length %d>>stream\n" % len(font_data)
+  pdf_data += b"4 0 obj\n"
+  pdf_data += b"<</Subtype /Type1/Length %d>>stream\n" % len(font_data)
   pdf_data += font_data
-  pdf_data += "\nendstream\n"
-  pdf_data += "endobj\n"
+  pdf_data += b"\nendstream\n"
+  pdf_data += b"endobj\n"
   
   # Construct the page contents object.
-  pdf_data += "5 0 obj\n"
-  pdf_data += "<< >>\n"
-  pdf_data += "stream\n"
-  pdf_data += "BT\n"
+  pdf_data += b"5 0 obj\n"
+  pdf_data += b"<< >>\n"
+  pdf_data += b"stream\n"
+  pdf_data += b"BT\n"
 
   segment_count = int(math.ceil(float(glyph_count) / GLYPHS_PER_SEGMENT))
-  for i in xrange(segment_count):
-    pdf_data += "  /CustomFont 12 Tf\n"
+  for i in range(segment_count):
+    pdf_data += b"  /CustomFont 12 Tf\n"
     if i == 0:
-      pdf_data += "  10 780 Td\n"
+      pdf_data += b"  10 780 Td\n"
     else:
-      pdf_data += "  0 -10 Td\n"
-    pdf_data += "  <%s> Tj\n" % "".join(map(lambda x: struct.pack(">H", x), (x for x in xrange(i * GLYPHS_PER_SEGMENT, (i + 1) * GLYPHS_PER_SEGMENT)))).encode("hex")
+      pdf_data += b"  0 -10 Td\n"
+    glyphs = b"".join(map(lambda x: struct.pack(">H", x), (x for x in range(i * GLYPHS_PER_SEGMENT, (i + 1) * GLYPHS_PER_SEGMENT))))
+    hex_str = "".join(["%02x" % b for b in glyphs]).encode("utf-8")
+    pdf_data += b"  <%s> Tj\n" % hex_str
 
-  pdf_data += "ET\n"
-  pdf_data += "endstream\n"
-  pdf_data += "endobj\n"
+  pdf_data += b"ET\n"
+  pdf_data += b"endstream\n"
+  pdf_data += b"endobj\n"
 
   # Construct the descendant font object.
-  pdf_data += "6 0 obj\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /FontDescriptor <<\n"
-  pdf_data += "    /Type /FontDescriptor\n"
-  pdf_data += "    /FontName /TestFont\n"
-  pdf_data += "    /Flags 5\n"
-  pdf_data += "    /FontBBox[0 0 10 10]\n"
-  pdf_data += "    /FontFile3 4 0 R\n"
-  pdf_data += "  >>\n"
-  pdf_data += "  /Type /Font\n"
-  pdf_data += "  /Subtype /Type1\n"
-  pdf_data += "  /BaseFont /TestFont\n"
-  pdf_data += ">>\n"
-  pdf_data += "endobj\n"
+  pdf_data += b"6 0 obj\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /FontDescriptor <<\n"
+  pdf_data += b"    /Type /FontDescriptor\n"
+  pdf_data += b"    /FontName /TestFont\n"
+  pdf_data += b"    /Flags 5\n"
+  pdf_data += b"    /FontBBox[0 0 10 10]\n"
+  pdf_data += b"    /FontFile3 4 0 R\n"
+  pdf_data += b"  >>\n"
+  pdf_data += b"  /Type /Font\n"
+  pdf_data += b"  /Subtype /Type1\n"
+  pdf_data += b"  /BaseFont /TestFont\n"
+  pdf_data += b">>\n"
+  pdf_data += b"endobj\n"
 
   # Construct the trailer.
-  pdf_data += "trailer\n"
-  pdf_data += "<<\n"
-  pdf_data += "  /Root 1 0 R\n"
-  pdf_data += ">>\n"
+  pdf_data += b"trailer\n"
+  pdf_data += b"<<\n"
+  pdf_data += b"  /Root 1 0 R\n"
+  pdf_data += b">>\n"
 
   # Write the output to disk.
   with open(argv[2], "w+b") as f:
